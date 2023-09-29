a8e8dad79d96ad3c1541c57b8be0179170c43595
Danil Boldyrev <daswerq123@gmail.com>
2023-09-29 18:28:11 +0300

    Init

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # easyYandexTranslate
-Using non-official API for translation with Yandex Translator
+https://www.npmjs.com/package/easyyandextranslate
+
+# Overview
+This is an unofficial wrapper for the Yandex Translate API, implemented in JavaScript. It allows you to translate large bodies of text or files while adhering to the API's text size limitation.
+
+# Install
+To install the necessary dependencies, run the following command:
+
+`npm i easyyandextranslate`
+
+# Usage
+You can use this wrapper to translate text, translate a file and write the translation to another file, or translate a file and return the translation as a string.
+
+## Here are examples of how to use these functions:
+
+`const { translate, translateFile, translateFileToText } = require('./path_to_your_file')`
+
+`translate('Hello world', 'ru').then(console.log);  // Привет мир`
+
+`translateFile('input.txt', 'output.txt', 'ru').then(() => {`
+`  console.log('File translated successfully');`
+`});`
+
+`translateFileToText('input.txt', 'ru').then(console.log);  // prints the translation of file content`
+
+# Functions
+**translate(text, targetLang, sourceLang = "")**
+
+Translates the given text from sourceLang to targetLang. If sourceLang isn't specified, the function will automatically detect the source language. Returns a promise that resolves to the translated text.
+
+**translateFile(inputFilePath, outputFilePath, targetLang, sourceLang = "")**
+Translates the content of the file at inputFilePath from sourceLang to targetLang and writes the translation to a new file at outputFilePath. If sourceLang isn't specified, the function will automatically detect the source language. Returns a promise that resolves when the operation is completed.
+
+**translateFileToText(inputFilePath, targetLang, sourceLang = "")**
+Translates the content of the file at inputFilePath from sourceLang to targetLang and returns the translation as a string. If sourceLang isn't specified, the function will automatically detect the source language. Returns a promise that resolves to the translated text.
+
+# Limitations
+This wrapper splits the input text into chunks to adhere to the Yandex Translate API's text size limitation. As a result, the translation might not be completely accurate if a sentence is split between two chunks.
+
+# Disclaimer
+This is an unofficial wrapper and is not affiliated with or endorsed by Yandex. The Yandex Translate API is a service provided by Yandex, and this wrapper merely provides an interface to access that service.
+
+
---
+++ index.cjs
@@ -0,0 +1,88 @@
+const axios = require('axios');
+const qs = require('querystring');
+const fs = require('fs');
+
+const MAX_CHARS = 500;
+
+async function translateChunk(text, sourceLang, targetLang) {
+    const lang = sourceLang ? `${sourceLang}-${targetLang}` : targetLang;
+
+    const data = qs.stringify({
+        'text': text,
+        'lang': lang
+    });
+
+    const config = {
+        method: 'post',
+        url: 'https://translate.yandex.net/api/v1/tr.json/translate?srv=ios&ucid=9676696D-0B56-4F13-B4D5-4A3DA2A3344D&sid=1A5A10A952AB4A3B82533F44B87EE696&id=1A5A10A952AB4A3B82533F44B87EE696-0-0',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: data
+    };
+
+    try {
+        const response = await axios(config);
+        if (response.data.code !== 200) throw new Error('Translation failed');
+        return response.data.text[0];
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function splitText(text) {
+    let chunks = [];
+    let startPos = 0;
+
+    while (startPos < text.length) {
+        let endPos = startPos + MAX_CHARS;
+        if (endPos < text.length) {
+            // Найти последнее полное предложение
+            endPos = text.lastIndexOf('. ', endPos);
+            if (endPos === -1) endPos = startPos + MAX_CHARS;
+            else endPos += 1;
+        }
+
+        chunks.push(text.slice(startPos, endPos));
+        startPos = endPos;
+    }
+
+    return chunks;
+}
+
+async function translate(text, targetLang, sourceLang = "") {
+    const chunks = splitText(text);
+    const translatedChunks = await Promise.all(chunks.map(chunk => translateChunk(chunk, targetLang, sourceLang)));
+    return translatedChunks.join(' ');
+}
+
+async function translateFile(inputFilePath, outputFilePath, targetLang, sourceLang = "") {
+    try {
+        const text = fs.readFileSync(inputFilePath, 'utf-8');
+        const translatedText = await translate(text, targetLang, sourceLang);
+        fs.writeFileSync(outputFilePath, translatedText, 'utf-8');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function translateFileToText(inputFilePath, targetLang, sourceLang = "") {
+    try {
+        const text = fs.readFileSync(inputFilePath, 'utf-8');
+        return await translate(text, targetLang, sourceLang = "");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+module.exports = {
+    translate,
+    translateFile,
+    translateFileToText,
+};
+
+// Использование функции
+// translate(`Tomonlar"AI bilan iPhone" yaratish loyihasini muhokama qilmoqdalar`, 'ru')
+//     .then(result => console.log(result))
+//     .catch(error => console.log(error));
